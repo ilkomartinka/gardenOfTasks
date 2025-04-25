5d75510035ea2c867557805744557714ded39bc7
Martina ILko <ilkomartina12@gmail.com>
2025-04-26 00:09:32 +0200

    The main page is nearly done, and the class for it is already created

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module org.example.gardenoftasks {
     requires javafx.controls;
     requires javafx.fxml;
     requires com.jfoenix;
+    requires java.desktop;
 
 
     opens org.example.gardenoftasks to javafx.fxml;
---
+++ src/main/java/org/example/gardenoftasks/HomePageController.java
@@ -0,0 +1,40 @@
+package org.example.gardenoftasks;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+import javafx.scene.control.Button;
+
+    public class HomePageController {
+
+        @FXML
+        private Button gardenBtn;
+
+        @FXML
+        private Button shopBtn;
+
+        @FXML
+        private Button toDoBtn;
+
+        @FXML
+        void addTask(ActionEvent event) {
+
+        }
+
+        @FXML
+        void switchToGarden(ActionEvent event) {
+
+        }
+
+        @FXML
+        void switchToShop(ActionEvent event) {
+
+        }
+
+        @FXML
+        void switchToTasks(ActionEvent event) {
+
+        }
+
+    }
+
