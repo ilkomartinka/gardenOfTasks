220b08c8ccae23074048ac23fc5c29fe4799b3fe
Martina ILko <ilkomartina12@gmail.com>
2025-05-11 00:32:34 +0200

    Added basic shop structure and elements

---
+++ src/main/java/org/example/gardenoftasks/ItemController.java
@@ -0,0 +1,18 @@
+package org.example.gardenoftasks;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+
+public class ItemController {
+
+    @FXML
+    private ImageView image;
+
+    @FXML
+    private Label nameLabel;
+
+    @FXML
+    private Label priceLabel;
+
+}
--- src/main/java/org/example/gardenoftasks/ShopController.java
+++ src/main/java/org/example/gardenoftasks/ShopController.java
@@ -1,4 +1,65 @@
 package org.example.gardenoftasks;
 
+import com.jfoenix.controls.JFXButton;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class ShopController {
+    private Switcher switcher;
+
+    @FXML
+    private JFXButton buyBtn;
+
+    @FXML
+    private HBox chosenPlantCard;
+
+    @FXML
+    private Button gardenBtn;
+
+    @FXML
+    private Button homeBtn;
+
+    @FXML
+    private ImageView plantImage;
+
+    @FXML
+    private Label plantNameLabel;
+
+    @FXML
+    private Label plantPriceLabel;
+
+    @FXML
+    private GridPane grid;
+
+    @FXML
+    private Button shopBtn;
+
+    @FXML
+    private ScrollPane scroll;
+
+    public ShopController() {
+        switcher = new Switcher();
+    }
+
+    @FXML
+    void goToGarden() throws IOException {
+        switcher.switchToScene((Stage) gardenBtn.getScene().getWindow(), "garden.fxml");
+    }
+
+    @FXML
+    void goToHome() throws IOException {
+        switcher.switchToScene((Stage) homeBtn.getScene().getWindow(), "mainPage.fxml");
+    }
+
+
+
 }
